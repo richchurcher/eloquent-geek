cf380b93d266c65ef9b463040e2964dcf885c29d
Rich Churcher <rich.churcher@gmail.com>
2015-04-13 08:15:53 +1200

    Add back base template for editing

--- root/home.go
+++ root/home.go
@@ -8,7 +8,6 @@ import (
 	"appengine"
 
 	"app"
-	"post"
 
 	"github.com/gorilla/sessions"
 )
@@ -19,21 +18,13 @@ func init() {
 }
 
 func home(w http.ResponseWriter, r *http.Request, c appengine.Context, s *sessions.Session) *app.Error {
-	p, err := post.All(c)
-	if err != nil {
-		return &app.Error{err, "Couldn't get posts from datastore.", http.StatusInternalServerError}
-	}
-
-	fm := template.FuncMap{
-		"trustedHTML": post.TrustedHTML,
-	}
 	t, err := template.New("home").
 		Funcs(fm).
 		ParseFiles("templates/base.html", "templates/home.html")
 	if err != nil {
 		return &app.Error{err, "Template parse error.", http.StatusInternalServerError}
 	}
-	if err := t.ExecuteTemplate(w, "base", p); err != nil {
+	if err := t.ExecuteTemplate(w, "base"); err != nil {
 		return &app.Error{err, "Template execute error.", http.StatusInternalServerError}
 	}
 	return nil
