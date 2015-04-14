0169cdd7c53f0117c60393f572c0cd1346db8084
Rich Churcher <rich.churcher@gmail.com>
2015-04-15 08:18:28 +1200

    Simple API working.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('eg', ['ngResource']);
+app.factory("Post", function($resource) {
+    return $resource("/post/");
+});
+
+app.controller("PostIndexCtrl", function ($scope, Post) {
+    Post.query(function(data) {
+        $scope.response = data;
+    });
+});
+
+app.controller("CreateCtrl", function ($scope, Post) {
+    Post.save({
+        title: "Title",
+        body: "Body",
+        tags: ["one", "two", "three"],
+    });
+});
+
