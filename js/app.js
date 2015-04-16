cb38701c51a85e5e4f53cd87ae4b917281e5111b
Rich Churcher <rich.churcher@gmail.com>
2015-04-16 19:22:20 +1200

    Create/POST works!

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 var app = angular.module("eg", ["ngResource", "postAPI"]);
 
+app.config(['$resourceProvider', function($resourceProvider) {
+  // Don't strip trailing slashes from calculated URLs
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
 var postAPI = angular.module("postAPI", ["ngResource"]);
 
 postAPI.factory("Post", ["$resource", 
-    function ($resource) {
-        return $resource("/post", {}, {});
+    function postFactory(resource) {
+        return resource("/post/:postId");
     }
 ]);
 
@@ -16,12 +21,24 @@ postAPI.controller("PostIndexCtrl", ["$scope", "Post",
     }
 ]);
 
-//postAPI.controller("CreateCtrl", ["$scope", "Post",
-    //function($scope, Post) {
-        //Post.save({
-            //title: "Title",
-            //body: "Body",
-            //tags: ["one", "two", "three"],
-        //});
-    //}
-//]);
+postAPI.controller("CreateCtrl", ["$scope", "Post",
+    function($scope, Post) {
+        Post.save({
+            title: "Title",
+            body: "Body",
+            tags: ["one", "two", "three"],
+        }, function (response) {
+            $scope.response = "API POST response: " + angular.toJson(response);
+        });
+    }
+]);
+
+postAPI.controller("DeleteCtrl", ["$scope", "Post",
+    function($scope, Post) {
+        Post.delete({
+            postId: 5629499534213120,
+        }), function (response) {
+            $scope.response = "API DELETE response: " + angular.toJson(response);
+        };
+    }
+]);
